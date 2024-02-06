3f3c021ba2170df3ca59cc07c7e75574d828749d
rtkay123 <rodney.kanjala@sybrin.com>
2024-02-06 12:47:47 +0200

    stack traces

--- lib/services/lumberjackGRPCService.d.ts
+++ lib/services/lumberjackGRPCService.d.ts
@@ -1,6 +1,10 @@
+import { type LumberjackClient } from '../helpers/proto/lumberjack/Lumberjack';
 import { type LogLevel } from '../helpers/proto/lumberjack/LogLevel';
+import { type LogMessage } from '../helpers/proto/lumberjack/LogMessage';
 export declare class LumberjackGRPCService {
-    #private;
+    client: LumberjackClient;
+    channel: string;
     constructor(host: string, channel: string);
+    makeMessage(message: string, level?: LogLevel, serviceOperation?: string, id?: string): LogMessage;
     log(message: string, level?: LogLevel, serviceOperation?: string, id?: string, callback?: (...args: unknown[]) => unknown): void;
 }
